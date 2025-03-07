c2305fdaeb60296ba9ee53e7ba84e4ccb621d5d2
Marek Sagan <marek.sagan@protonmail.com>
2025-03-07 08:47:03 +0100

    Refactoring

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .additionalMessageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
+    }
+}
